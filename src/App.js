eaa8b9170969cf873e0dbe4ede2ea85af15fca1f
Ammar Qureshi <qureshiaq2003@gmail.com>
2022-08-25 23:27:50 +0500

    Push

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@ import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Screen/Home-Page";
 import Facbook from "./Screen/Facebook-Home";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [post,setPost] = useState([])
+  useEffect(()=>{
+    console.log("Chala bhai-->>>")
+    fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(res => res.json())
+    .then(res => setPost(res))
+  },[])
+   
   return (
     <>
-      <Facbook />
+    <Home />
+    {/* <h1>{post}</h1> */}
+    <ol>
+      {post.map((item)=>{
+        return(
+          <>
+          <li>{item.title}</li>
+          </>
+        )
+      })}
+    </ol>
+    <button onClick={()=>{setPost([])}}>Empty List</button>
+      {/* <Facbook /> */}
       {/* <Home /> */}
     </>
   );
